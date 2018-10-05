45525de454c16dbcefd009654abefa6b4230aff5
muhrik/majo/mario uhrik <433501@mail.muni.cz>
2018-10-05 20:47:35 +0200

    stat.py now reads the data from scorelib.txt and saves them into dictionaries

--- 01-text/stat.py
+++ 01-text/stat.py
@@ -1,1 +1,85 @@
 import sys
+import re
+
+printNumberRE = re.compile(r"Print Number: (.*)")
+composerRE = re.compile(r"Composer: (.*)")
+titleRE = re.compile(r"Title: (.*)")
+genreRE = re.compile(r"Genre: (.*)")
+keyRE = re.compile(r"Key: (.*)")
+CompositionYearRE = re.compile(r"Composition Year: (.*)")
+PublicationYearRE = re.compile(r"Publication Year: (.*)")
+EditionRE = re.compile(r"Edition: (.*)")
+EditorRE = re.compile(r"Editor: (.*)")
+VoiceRE = re.compile(r"Voice (.*)")
+PartitureRE = re.compile(r"Partiture: (.*)")
+IncipitRE = re.compile(r"Incipit: (.*)")
+
+file = open('scorelib.txt', 'r')
+
+composerDict = {}
+titleDict = {}
+genreDict = {}
+keyDict = {}
+compositionYearDict = {}
+publicationYearDict = {}
+editionDict = {}
+editorDict = {}
+voiceDict = {}
+partitureDict = {}
+incipitDict = {}
+
+m = None;
+for line in file:
+	if (m == None):
+		m = printNumberRE.match(line)
+		if (m != None):
+			printNumber = m.group(1)
+	if (m == None):
+		m = composerRE.match(line)
+		if (m != None):
+			composerDict[printNumber] = m.group(1)
+	if (m == None):
+		m = titleRE.match(line)
+		if (m != None):
+			titleDict[printNumber] = m.group(1)
+	if (m == None):
+		m = genreRE.match(line)
+		if (m != None):
+			genreDict[printNumber] = m.group(1)
+	if (m == None):
+		m = keyRE.match(line)
+		if (m != None):
+			keyDict[printNumber] = m.group(1)
+	if (m == None):
+		m = CompositionYearRE.match(line)
+		if (m != None):
+			compositionYearDict[printNumber] = m.group(1)
+	if (m == None):
+		m = PublicationYearRE.match(line)
+		if (m != None):
+			publicationYearDict[printNumber] = m.group(1)
+	if (m == None):
+		m = EditionRE.match(line)
+		if (m != None):
+			editionDict[printNumber] = m.group(1)
+	if (m == None):
+		m = EditorRE.match(line)
+		if (m != None):
+			editorDict[printNumber] = m.group(1)
+	if (m == None):
+		m = VoiceRE.match(line)
+		if (m != None):
+			s = m.group(1).split(":")
+			if (s[0] == "1"):
+				voiceDict[printNumber] = {}
+			voiceDict[printNumber][s[0]] = s[1]
+	if (m == None):
+		m = PartitureRE.match(line)
+		if (m != None):
+			partitureDict[printNumber] = m.group(1)
+	if (m == None):
+		m = IncipitRE.match(line)
+		if (m != None):
+			incipitDict[printNumber] = m.group(1)
+
+
