7b588dc78783739a6fef7d9c41720fa7a3ba7071
muhrik/majo/mario uhrik <433501@mail.muni.cz>
2018-10-06 18:21:47 +0200

    01-text is now finished

--- 01-text/stat.py
+++ 01-text/stat.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 import re
 
@@ -14,7 +15,15 @@ VoiceRE = re.compile(r"Voice (.*)")
 PartitureRE = re.compile(r"Partiture: (.*)")
 IncipitRE = re.compile(r"Incipit: (.*)")
 
-file = open('scorelib.txt', 'r')
+if (len(sys.argv) == 1):
+	print("The first command line argument must be a text file for the script to read.")
+	print("E.g. scorelib.txt")
+	sys.exit()
+if (len(sys.argv) == 2):
+	print("The second command line argument must be either \'composer\' or \'century\'.")
+	print("No second command line argument has been received.")
+	sys.exit()
+file = open(sys.argv[1], 'r')
 
 composerDict = {}
 titleDict = {}
@@ -33,51 +42,89 @@ for line in file:
 	if (m == None):
 		m = printNumberRE.match(line)
 		if (m != None):
-			printNumber = m.group(1)
+			printNumber = m.group(1).strip()
 	if (m == None):
 		m = composerRE.match(line)
 		if (m != None):
-			composerDict[printNumber] = m.group(1)
+			s = m.group(1).strip().split(";")
+			composerDict[printNumber] = {}
+			for i in range(0, len(s)):
+				composerDict[printNumber][i] = s[i].strip()
+				composerDict[printNumber][i] = re.sub("[(].*[0-9].*[)]", "", composerDict[printNumber][i]).strip()
 	if (m == None):
 		m = titleRE.match(line)
 		if (m != None):
-			titleDict[printNumber] = m.group(1)
+			titleDict[printNumber] = m.group(1).strip()
 	if (m == None):
 		m = genreRE.match(line)
 		if (m != None):
-			genreDict[printNumber] = m.group(1)
+			genreDict[printNumber] = m.group(1).strip()
 	if (m == None):
 		m = keyRE.match(line)
 		if (m != None):
-			keyDict[printNumber] = m.group(1)
+			keyDict[printNumber] = m.group(1).strip()
 	if (m == None):
 		m = CompositionYearRE.match(line)
 		if (m != None):
-			compositionYearDict[printNumber] = m.group(1)
+			year = re.search("[1-3][0-9]{3}", m.group(1))
+			if (year != None):
+				compositionYearDict[printNumber] = year.group(0).strip()
+			else:
+				century = re.search("[1][0-9][t][h]", m.group(1))
+				if (century != None):
+					compositionYearDict[printNumber] = century.group(0).strip().strip("th").strip()
 	if (m == None):
 		m = PublicationYearRE.match(line)
 		if (m != None):
-			publicationYearDict[printNumber] = m.group(1)
+			year = re.search("[1-3][0-9]{3}", m.group(1))
+			if (year != None):
+				publicationYearDict[printNumber] = year.group(0).strip()
 	if (m == None):
 		m = EditionRE.match(line)
 		if (m != None):
-			editionDict[printNumber] = m.group(1)
+			editionDict[printNumber] = m.group(1).strip()
 	if (m == None):
 		m = EditorRE.match(line)
 		if (m != None):
-			editorDict[printNumber] = m.group(1)
+			editorDict[printNumber] = m.group(1).strip()
 	if (m == None):
 		m = VoiceRE.match(line)
 		if (m != None):
-			s = m.group(1).split(":")
+			s = m.group(1).strip().split(":")
 			if (s[0] == "1"):
 				voiceDict[printNumber] = {}
-			voiceDict[printNumber][s[0]] = s[1]
+			voiceDict[printNumber][s[0]] = s[1].strip()
 	if (m == None):
 		m = PartitureRE.match(line)
 		if (m != None):
-			partitureDict[printNumber] = m.group(1)
+			partitureDict[printNumber] = m.group(1).strip()
 	if (m == None):
 		m = IncipitRE.match(line)
 		if (m != None):
-			incipitDict[printNumber] = m.group(1)
+			incipitDict[printNumber] = m.group(1).strip()
+
+if (sys.argv[2] == "composer"):
+	composerCountDict = {}
+	for k, v in composerDict.items():
+		for k2, v2 in v.items():
+			if (composerCountDict.get(v2) == None):
+				composerCountDict[v2] = 1
+			composerCountDict[v2] = composerCountDict[v2] + 1
+	for k, v in composerCountDict.items():
+		print(k + ": " + str(v))
+
+if (sys.argv[2] == "century"):
+	centCountDict = {}
+	for cntr in range(16,21):
+		centCountDict[cntr] = 0;
+		minYear = (cntr - 1) * 100
+		maxYear = (cntr) * 100
+		for k, v in compositionYearDict.items():
+			if (v.isdigit()):
+				year = int(v)
+				if (year <= maxYear and year > minYear):
+					centCountDict[cntr] = centCountDict[cntr] + 1
+				if (year == cntr):
+					centCountDict[cntr] = centCountDict[cntr] + 1
+	for k, v in centCountDict.items():
+		print(str(k) + "th century: " + str(v))
