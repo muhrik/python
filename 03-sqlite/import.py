eb9dea57a32f85b31e00ee192533951d0eebaa5a
xuhrik <433501@mail.muni.cz>
2018-10-09 17:32:45 +0200

    planned out the solution for 03-sqlite

--- 03-sqlite/import.py
+++ 03-sqlite/import.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import sqlite3
+import scorelib.py
 
 if (len(sys.argv) == 1):
 	print("The first command line argument must be a text file for the script to read.")
@@ -10,6 +11,13 @@ if (len(sys.argv) == 2):
 	print("The second command line argument is the output SQLite file.")
 	sys.exit()
 
-conn = sqlite3.connect("scorelib.dat")
+load(sys.argv[1])
+
+conn = sqlite3.connect(sys.argv[2])
+cur = conn.cursor()
 
 #TODO continue
+#TODO 1) Create relevant tables using the schematics from scorelib.sql
+#TODO 2) For all existing objects from load(), insert them into the tables
+
+conn.commit()
