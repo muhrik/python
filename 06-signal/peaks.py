94d7cba87e8b533d41097e5095ec3c3ee438c8af
muhrik/majo/mario uhrik <433501@mail.muni.cz>
2018-11-12 19:24:58 +0100

    finally fixed 06-signal, it should be working as intended now

--- 06-signal/peaks.py
+++ 06-signal/peaks.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 #author: Mario Uhrik 433501
 import sys
-import numpy
-import re
+import numpy as np
 import wave
 import struct
 
@@ -15,40 +14,43 @@ file = wave.open(sys.argv[1])
 numFrames = file.getnframes()
 numChannels = file.getnchannels()
 numSamples = numFrames * numChannels
-
+frameRate = file.getframerate()
 data = file.readframes(numFrames)
+file.close()
 
 nums = struct.unpack("%ih" % numSamples, data)
 
-#for num in nums: #DEBUG
-#	print("Num " + str(num)) #DEBUG
+minPeakIndex = -1
+maxPeakIndex = -1
 
-a = numpy.asarray(nums)
-output = numpy.fft.rfft(a)
-absOutput = numpy.absolute(output)
+for windowIndex in range(0, len(nums) / (frameRate*numChannels) ):
+	windowNumsIndexLeft = windowIndex * frameRate*numChannels
+	windowNumsIndexRight = windowNumsIndexLeft + frameRate*numChannels
+	windowNums = nums[windowNumsIndexLeft:windowNumsIndexRight]
+	a = np.asarray(windowNums)
+	
+	if (numChannels == 2):
+		left = a[::2]
+		right = a[1::2]
+		wAverage = (left + right) / 2
+		out = np.fft.rfft(wAverage)
+	if (numChannels == 1):
+		out = np.fft.rfft(a)
+	absOut = np.absolute(out)
 
-absOutputAverage = numpy.average(absOutput)
-absOutputAverageTwenty = absOutputAverage * 20
+	absOutAverage = np.average(absOut)
+	absOutAverageTwenty = absOutAverage * 20
 
-minPeak = -1
-maxPeak = -1
-for i in range(0, len(absOutput) - 1):
-	cn = absOutput[i]
-	#print("Iteration " + str(i) + ": " + str(cn)) #DEBUG
-	if (cn >= absOutputAverageTwenty):
-		if (minPeak == -1 and maxPeak == -1):
-			minPeak = cn
-			maxPeak = cn
-		if (minPeak > cn):
-			minPeak = cn
-		if (maxPeak < cn):
-			maxPeak = cn
+	for i in range(0, len(absOut)):
+		cn = absOut[i]
+		if (cn >= absOutAverageTwenty):
+			if (minPeakIndex == -1):
+				minPeakIndex = i
+			maxPeakIndex = i
 
-if (minPeak == -1 and maxPeak == -1):
+
+if (minPeakIndex == -1 and maxPeakIndex == -1):
 	print("no peaks")
 else:
-	print("low = " + str(minPeak) + ", high = " + str(maxPeak))
-#print("AVG: " + str(absOutputAverage)) #DEBUG
-#print("AVG20: " + str(absOutputAverageTwenty)) #DEBUG
-
+	print("low = " + str(minPeakIndex) + ", high = " + str(maxPeakIndex))
 
