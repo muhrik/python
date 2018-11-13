5c73a11a6dade5ce8bb65a98058212119acb890a
muhrik/majo/mario uhrik <433501@mail.muni.cz>
2018-11-13 19:56:25 +0100

    08-student/stat.py should be working 100% correctly

--- 08-student/stat.py
+++ 08-student/stat.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import sys
-import numpy
+import re
+import numpy as np
 import pandas as pd
+import json
 
 if (len(sys.argv) == 1):
 	print("The first command line argument must be a csv file with student data.")
@@ -16,32 +18,48 @@ df = pd.read_csv(sys.argv[1])
 columnNames = list(df)
 
 
-meanMap = {}
-medianMap = {}
-firstQMap = {}
-secondQMap = {}
+dataMap = {}
 if (sys.argv[2] == "dates"):
 	for columnName in columnNames:
 		if (columnName == "student"):
-			continue:
-		mean = df[columnName].mean()
-		median = df[columnName].median()
-		firstQ = df[columnName].quantile(0.25)
-		lastQ = df[columnName].quantile(0.75)
+			continue
+		date = 	re.sub(r"[/][0-9][0-9]", "", columnName)
+
+		subDataMap = {}
+		subDataMap["mean"] = df.filter(like=date).mean().mean()
+		subDataMap["median"] = df.filter(like=date).median().median()
+		subDataMap["first"] = df.filter(like=date).quantile(0.25).quantile(0.25)
+		subDataMap["last"] = df.filter(like=date).quantile(0.75).quantile(0.75)
+		subDataMap["passed"] = df.filter(like=date).pct_change().gt(0).sum().sum()
+
+		dataMap[date] = subDataMap
 if (sys.argv[2] == "deadlines"):
+	for columnName in columnNames:
+		if (columnName == "student"):
+			continue
 
+		subDataMap = {}
+		subDataMap["mean"] = df[columnName].mean()
+		subDataMap["median"] = df[columnName].median()
+		subDataMap["first"] = df[columnName].quantile(0.25)
+		subDataMap["last"] = df[columnName].quantile(0.75)
+		subDataMap["passed"] = df[columnName].pct_change().gt(0).sum()
+
+		dataMap[columnName] = subDataMap
 if (sys.argv[2] == "exercises"):
+	for columnName in columnNames:
+		if (columnName == "student"):
+			continue
+		exercise = re.sub(r"[0-9][0-9][0-9][0-9][-][0-9][0-9][-][0-9][0-9]", "", columnName)
+		exerciseNum = re.sub(r"[/]", "", exercise)
 
+		subDataMap = {}
+		subDataMap["mean"] = df.filter(like=exercise).mean().mean()
+		subDataMap["median"] = df.filter(like=exercise).median().median()
+		subDataMap["first"] = df.filter(like=exercise).quantile(0.25).quantile(0.25)
+		subDataMap["last"] = df.filter(like=exercise).quantile(0.75).quantile(0.75)
+		subDataMap["passed"] = df.filter(like=exercise).pct_change().gt(0).sum().sum()
 
+		dataMap[exerciseNum] = subDataMap
 
-#studentMap = {} # student data submaps mapped onto their IDs
-#for index, row in df.iterrows():
-#	studentDataMap = {} # student data submaps where their data is mapped onto their keys
-#	studentID = 0
-#	for columnName in columnNames:
-#		if (columnName == "student"):
-#			studentID = row[columnName]
-#			continue
-#		studentDataMap[columnName] = row[columnName]
-#	studentMap[studentID] = studentDataMap
-
+print(json.dumps(dataMap, sort_keys=True, indent=4, separators=(',', ': ')))
